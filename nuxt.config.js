cfb13217302e80876a6188bc43ec706fd284b795
Virginia Wanjiru <virginiashee55@gmail.com>
2024-12-03 22:27:50 +0300

    dist

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,6 +1,9 @@
 export default {
   buildModules: ['@nuxtjs/tailwindcss'],
   css: ['@/assets/styles/main.css'],
+  build: {
+    outDir: 'dist' // Ensure this is 'dist' or your custom directory
+  }
   
 
 }
