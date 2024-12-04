f5460b168a317ce1cd12a44dbc6062ab26b26b90
Virginia Wanjiru <virginiashee55@gmail.com>
2024-12-04 09:48:25 +0300

    nitro

--- nuxt.config.js
+++ nuxt.config.js
@@ -3,7 +3,11 @@ export default {
   css: ['@/assets/styles/main.css'],
   build: {
     outDir: 'dist' // Ensure this is 'dist' or your custom directory
-  }
+  },
+
+  nitro: {
+    preset: 'vercel',
+  },
   
 
 }
